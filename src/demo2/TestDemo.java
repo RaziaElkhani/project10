8ca774d5a5462a89f3fc4a80af4b304f4557179f
elkha <elkha@10.0.0.142>
2025-01-30 12:49:00 -0800

    divide method

---
+++ src/demo2/TestDemo.java
@@ -0,0 +1,15 @@
+package demo2;
+
+public class TestDemo {
+public static void main(String[] args) {
+	divide(20, 34);
+	divide(3,5);
+	
+}	
+	
+public static void divide(int a,int b) {
+	System.out.println(a/b);
+}	
+
+}	
+
